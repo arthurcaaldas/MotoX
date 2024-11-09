90a92a9a74e3d28e63363b13a107075425499b55
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-09 09:41:07 -0300

    ajustando estrutura e classes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,20 +6,21 @@ const Footer = () => {
       <img className="mb-2" src="src\assets\images\motox.png" alt="logo marca" />
 
       <div>
-        <div className="mb-3">
+        <div className="mb-2">
           <h4>Endereço</h4>
           <p>Rua Alcídes Ramos de Lima, 4, Jacintinho </p>
           <p>Maceió-AL, 57041-020</p>
         </div>
         
-        <div className="mb-3">
-          <p><span>Email:</span> motox@gmail.com</p>
+        <div className="mb-2">
+          <h4>Contato</h4>
           <p><span>Telefone:</span> (82) 98872-3172</p>
+          <p><span>Email:</span> motoxmcz@gmail.com</p>
           <p><span>Instagram:</span> @motoxmcz</p>
         </div>
 
-        <div className="mb-2">
-          <ul>
+        <div className="flex justify-center mb-2">
+          <ul className="flex flex-direction-column justify-space-between">
             <li>
               <a className="flex align-items-center mb-1" target="_blank" href="https://api.whatsapp.com/send/?phone=558288723172&text&type=phone_number&app_absent=0">
                 <img className="mr-05" src="src\assets\images\icons\icon-whatsapp.png" alt="icon whatsapp" />
@@ -33,7 +34,7 @@ const Footer = () => {
               <a className="flex align-items-center" target="_blank" href="https://www.instagram.com/motoxmcz/">
                 <img className="mr-05" src="src\assets\images\icons\icon-instagram.png" alt="icon instagram" />                
                 <div>
-                  <p>Siga!</p>
+                  <p>Siga-nos!</p>
                   <p>nosso instagram</p>
                 </div>
               </a>
@@ -42,8 +43,8 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="text-center">
-        <p>TODOS OS DIREITOS RESERVADOS &copy; 2015 - MOTOX</p>
+      <div>
+        <p>TODOS OS DIREITOS RESERVADOS &copy; MOTOX - 2013</p>
         <p>FEITO POR <a target="_blank" href="https://www.linkedin.com/in/arthurcaaldas/">ARTHUR CALDAS</a></p>
       </div>
     </footer>
