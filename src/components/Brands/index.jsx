5d97a90427bfefe7f10445927f2cfb5b2c881fa8
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-11 20:23:55 -0300

    Iniciando component Brands

---
+++ src/components/Brands/index.jsx
@@ -0,0 +1,53 @@
+import './Brands.module.css';
+// import styles from './Brands.module.css'
+
+
+const Brands = () => {
+  return (
+    <div className="pb-4">
+      <h2 className="text-center pb-2">Trabalhamos com as melhores marcas</h2>
+      <section>
+       <ul>
+        <li>
+          <img src="src/assets/images/brands/motul.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/mobil.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/lubrax.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/riffel.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/motul.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/mobil.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/lubrax.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/riffel.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/motul.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/mobil.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/lubrax.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/riffel.png" alt="" />
+        </li>
+       </ul>
+      </section>
+    </div>
+  )
+}
+
+export default Brands
