081f95378a5a41539e930d51b806865d93e9a2df
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-11 10:55:49 -0300

    Adicionando App.jsx para routers

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// import Home from "./pages/Home"
+// import SignUpPage from "./pages/SignUpPage"
+// import Sobre from "./pages/Sobre"
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={ <Home /> } />
+//         <Route path="/SignUpPage" element={<SignUpPage />} />
+//         <Route path="/sobre" element={ <Sobre /> } />
+//       </Routes>
+//     </Router>
+//   )
+// }
+
+// export default App
