770cc7583145fce0b9417dfd66d3b4ce282b23ee
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-12 09:46:12 -0300

    adicionando logo e ajustando component brands

--- src/components/Brands/index.jsx
+++ src/components/Brands/index.jsx
@@ -21,28 +21,40 @@ const Brands = () => {
           <img src="src/assets/images/brands/riffel.png" alt="" />
         </li>
         <li>
-          <img src="src/assets/images/brands/motul.png" alt="" />
+          <img src="src/assets/images/brands/norisk.png" alt="" />
         </li>
         <li>
-          <img src="src/assets/images/brands/mobil.png" alt="" />
+          <img src="src/assets/images/brands/hamp.png" alt="" />
         </li>
         <li>
-          <img src="src/assets/images/brands/lubrax.png" alt="" />
+          <img src="src/assets/images/brands/scud.png" alt="" />
         </li>
         <li>
-          <img src="src/assets/images/brands/riffel.png" alt="" />
+          <img src="src/assets/images/brands/did.png" alt="" />
         </li>
         <li>
-          <img src="src/assets/images/brands/motul.png" alt="" />
+          <img src="src/assets/images/brands/x11.png" alt="" />
         </li>
         <li>
-          <img src="src/assets/images/brands/mobil.png" alt="" />
+          <img src="src/assets/images/brands/vipal.png" alt="" />
         </li>
         <li>
-          <img src="src/assets/images/brands/lubrax.png" alt="" />
+          <img src="src/assets/images/brands/cobreq.png" alt="" />
         </li>
         <li>
-          <img src="src/assets/images/brands/riffel.png" alt="" />
+          <img src="src/assets/images/brands/cofap.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/potenza.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/levorin.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/ngk.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/ls2.png" alt="" />
         </li>
        </ul>
       </section>
