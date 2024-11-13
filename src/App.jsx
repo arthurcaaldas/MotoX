17ec65f29ab3e5a66bd9b113e34a8772eaaeaf68
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-13 10:07:21 -0300

    Adicionando location e seletor com background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 import Home from "./pages/Home"
 import ServiceParts from "./pages/ServicesParts"
+import History from "./pages/History"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/ServiceParts" element={ <ServiceParts /> } />
+        <Route path="/History" element={ <History /> } />
+        <Route path="/Contact" element={ <Contact /> } />
       </Routes>
     </Router>
   )
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,37 +1,43 @@
-import './Header.module.css'
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import styles from './Header.module.css';
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <header className="flex justify-center">
       <div className="flex justify-space-between">
-        <img width="100px" height="100px" src="src\assets\images\logo-sem-fundo.png" alt="logo marca" />
+        
+        <Link to="/">
+          <img width="100px" height="100px" src="src/assets/images/logo-sem-fundo.png" alt="logo marca" />
+        </Link>
+
         <nav>
           <div>
             <ul>
-              <li>
-               <Link to="/">Home</Link>
+              <li className={location.pathname === "/" ? `${styles.active}` : ""}>
+                <Link to="/">Home</Link>
               </li>
-              <li>
+              <li className={location.pathname === "/serviceParts" ? `${styles.active}` : ""}>
                 <Link to="/serviceParts">Serviços</Link>
               </li>
-              <li>
-               <Link to="/">História</Link>
+              <li className={location.pathname === "/history" ? `${styles.active}` : ""}>
+                <Link to="/history">História</Link>
               </li>
-              <li>
-                <Link to="/">Contato</Link>
+              <li className={location.pathname === "/contact" ? `${styles.active}` : ""}>
+                <Link to="/contact">Contato</Link>
               </li>
             </ul>
 
             <ul className="flex">
               <li>
                 <a target="_blank" href="https://api.whatsapp.com/send/?phone=558288723172&text&type=phone_number&app_absent=0">
-                  <img src="src\assets\images\icons\icon-whatsapp.png" alt="icon whatsapp" />
+                  <img src="src/assets/images/icons/icon-whatsapp.png" alt="icon whatsapp" />
                 </a>
               </li>
               <li>
                 <a target="_blank" href="https://www.instagram.com/motoxmcz/">
-                  <img src="src\assets\images\icons\icon-instagram.png" alt="icon instagram" />
+                  <img src="src/assets/images/icons/icon-instagram.png" alt="icon instagram" />
                 </a>
               </li>
             </ul>
@@ -39,7 +45,7 @@ const Header = () => {
         </nav>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
