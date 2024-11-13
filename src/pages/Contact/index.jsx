9c9cb62b453c80af5ba1e741a4915f0b84cc6130
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-13 10:06:27 -0300

    iniciando page contact

---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,20 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer"
+import './Contact.css'
+
+
+const Contact = () => {
+  return (
+    <body>
+      <Header></Header>
+      
+      <main>
+
+      </main>
+
+      <Footer></Footer>
+    </body>
+  )
+}
+
+export default Contact
