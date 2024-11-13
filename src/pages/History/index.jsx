94704b1c74f545675966e91f472660a89e9afb55
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-13 10:06:47 -0300

    iniciando page History

---
+++ src/pages/History/index.jsx
@@ -0,0 +1,20 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer"
+import './History.css'
+
+
+const History = () => {
+  return (
+    <body>
+      <Header></Header>
+      
+      <main>
+
+      </main>
+
+      <Footer></Footer>
+    </body>
+  )
+}
+
+export default History
