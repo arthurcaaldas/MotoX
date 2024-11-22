8f5ce65708554d59e7a011a0851416024cd3477a
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-22 10:01:14 -0300

    Iniciando novo component

---
+++ src/components/Milestones/index.jsx
@@ -0,0 +1,62 @@
+import styles from './Milestones.module.css';
+
+const Milestones = () => {
+  return (
+    <div className="pb-4">
+      <section className={`${styles.milestones}`}>
+       <ul>
+        <li>
+          <img src="src/assets/images/brands/motul.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/mobil.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/lubrax.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/riffel.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/norisk.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/hamp.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/scud.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/did.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/x11.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/vipal.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/cobreq.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/cofap.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/potenza.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/levorin.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/ngk.png" alt="" />
+        </li>
+        <li>
+          <img src="src/assets/images/brands/ls2.png" alt="" />
+        </li>
+       </ul>
+      </section>
+    </div>
+  )
+}
+
+export default Milestones
