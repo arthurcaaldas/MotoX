59384796adba87d1845554aefedc674387268805
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-22 10:47:57 -0300

    alterando texto e adicionando espaçamento

--- src/components/Trajectory/index.jsx
+++ src/components/Trajectory/index.jsx
@@ -4,7 +4,7 @@ const Trajectory = () => {
  
   return (
     <section className={`${styles.trajectory}`}>
-      <h2 className={`${"py-2 px-3 text-center"}`}>Conheça nossa História!</h2>
+      <h2 className={`${"py-3 px-3 text-center"}`}>Conheça nossa História!</h2>
       <p className={`${"pb-2 px-2"}`}>
         A Moto X peças e serviços surgiu em 2013 com a missão de atender o mercado 
         no segmento de serviços para motocicletas, comercializando produtos e 
@@ -12,7 +12,7 @@ const Trajectory = () => {
         Suzuki, uma variedade de outras marcas e seus modelos.
       </p>
       <p className={`${"pb-2 px-2"}`}>
-        A Moto X peças e serviços foi fundada por Adalberto Santos, que sempre foi 
+        Foi fundada por Adalberto Santos, que sempre foi 
         um entusiasta por motocicletas, e é um profissional com vasta experiência 
         no mercado, além da sua expertise, a empresa conta com uma equipe 
         qualificada de profissionais, que focam em desempenhar seus trabalhos para 
@@ -21,7 +21,7 @@ const Trajectory = () => {
         poder realizar variados tipos de serviços para motocicletas de diversos 
         portes.
       </p>
-      <p className={`${"pb-2 px-2"}`}>
+      <p className={`${"pb-2 mb-3 px-2"}`}>
         Desde sua inauguração, a empresa localiza-se na rua Alcides Ramos de Lima, 
         no bairro do Jacintinho, em Maceió, um dos principais locais de referência 
         do segmento de serviços para motocicletas na cidade. São mais de 10 anos de 
