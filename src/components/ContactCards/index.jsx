918698855cf0992115c33c89f4b46a3a03bdd5da
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-23 14:19:46 -0300

    ajustando cards de contact

--- src/components/ContactCards/index.jsx
+++ src/components/ContactCards/index.jsx
@@ -4,34 +4,42 @@ const ContactCards = () => {
   return (
     <section className={`${styles.contactCards} ${"pt-5"}`}>
        <ul>
-        <li className={`${"flex-direction-column justify-space-between"}`}>
+        <li className={`${"py-2 flex-direction-column justify-space-between"}`}>
+          <a target="_blank" href="https://maps.app.goo.gl/L9rgDagt5HZvydhA7">
+            <img src="src/assets/images/icons/icon-loc.png" alt="" />
+          </a>
           <div className={`${"text-center"}`}>
             <h4>Localização</h4>
             <p>Rua Alcídes Ramos de Lima, 65, Jacintinho</p>
             <p>Maceió-AL, 57041-020</p>
           </div>
-          <img src="src/assets/images/icons/icon-loc.png" alt="" />
         </li>
         <li className={`${"flex-direction-column justify-space-between"}`}>
+          <a target="_blank" href="https://www.instagram.com/motoxmcz/">
+            <img src="src/assets/images/icons/icon-instagram.png" alt="" />
+          </a>
           <div className={`${"text-center"}`}>
             <h4>Siga no instagram</h4>
             <p>@Motoxmcz</p>
           </div>
-          <img src="src/assets/images/icons/icon-instagram.png" alt="" />
         </li>
-        <li className={`${"flex-direction-column justify-space-between"}`}>
+        <li className={`${"py-2 flex-direction-column justify-space-between"}`}>
+          <a target="_blank" href="https://api.whatsapp.com/send/?phone=558288723172&text&type=phone_number&app_absent=0">
+            <img src="src/assets/images/icons/icon-whatsapp.png" alt="" />
+          </a>
           <div className={`${"text-center"}`}>
             <h4>Fale conosco</h4>
             <p>(082) 98872-3172</p>
           </div>
-          <img src="src/assets/images/icons/icon-whatsapp.png" alt="" />
         </li>
         <li className={`${"flex-direction-column justify-space-between"}`}>
+          <a href="">
+            <img src="src/assets/images/icons/icon-facebook.png" alt="" />
+          </a>
           <div className={`${"text-center"}`}>
             <h4>Curta no facebook</h4>
             <p>@Motoxmcz</p>
           </div>
-          <img src="src/assets/images/icons/icon-facebook.png" alt="" />
         </li>
        </ul>
       </section>
