f7f5817e8c7f8a36204c91348ee76e2aa5f428a9
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-11-30 21:11:07 -0300

    adicionando carousel a pages

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
 import Header from "../../components/Header";
 import Carousel from "../../components/Carousel";
 import Cards from "../../components/Cards"
@@ -5,6 +8,11 @@ import Brands from "../../components/Brands"
 import Footer from "../../components/Footer"
 import './Home.css'
 
+const images = [
+  "https://via.placeholder.com/1200x600?text=Image+1",
+  "https://via.placeholder.com/1200x600?text=Image+2",
+  "https://via.placeholder.com/1200x600?text=Image+3",
+];
 
 const Home = () => {
   return (
@@ -12,7 +20,7 @@ const Home = () => {
       <Header></Header>
       
       <main className={`${"mt-header"}`}>
-        <Carousel></Carousel>
+        <Carousel images={images} />
         <Cards></Cards>
         <Brands></Brands>
       </main>
